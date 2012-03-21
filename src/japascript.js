45a508933c0d11dbd02705da5eb40170240ce87d
phi <phi1618jp@gmail.com>
2012-03-21 23:00:27 +0900

    add main script

---
+++ src/japascript.js
@@ -0,0 +1,13 @@
+/**
+ * phi
+ */
+
+(function(np){
+    
+    np.ウィンドウ = window;
+    np.警告 = alert;
+    np.確認 = confirm;
+    np.ドキュメント = document;
+    np.ドキュメント.書く = document.write;
+    
+})(window);
